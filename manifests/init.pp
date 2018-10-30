041a51e0a8b44917997911184fea21a84525e47c
Gene Liverman <gene+06108928@technicalissues.us>
2018-10-29 20:25:44 -0400

    Ran pdk new class demo_sensitive_puppet

---
+++ manifests/init.pp
@@ -0,0 +1,8 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include demo_sensitive_puppet
+class demo_sensitive_puppet {
+}
